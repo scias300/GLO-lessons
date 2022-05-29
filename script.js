37b4658886473bfd9cb1b57626c4e1567b0bf1e7
Alexander Pashkov <scias300@gmail.com>
2022-05-29 14:31:24 +0300

    Выполнил задание второго урока

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-let title,
-    screens,
-    screenPrice,
-    rollback,
-    fullPrice,
-    adaptive;
+let title = "project",
+    screens = "Простые, Сложные, Интерактивные",
+    screenPrice = 12345,
+    rollback = 50,
+    fullPrice = 54321,
+    adaptive = true;
 alert('Любой текст!');
 console.log('Сообщение с любым текстом');
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " долларов");
+console.log("Стоимость разработки сайта " + fullPrice + " долларов");
+console.log(screens.toLowerCase().split(', '));
+console.log("Процент отката посреднику за работу " + fullPrice * (rollback / 100));
